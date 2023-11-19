0701c5d4b7f003b753c295858eb09acc48f6243c
CalvinAJK <calvinaujiakyn@gmail.com>
2023-11-19 05:14:15 +0000

    Add project files.

---
+++ Browse API/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using Browse_API.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+[Route("api/products")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    private readonly ProductContext _context;
+
+    public ProductsController(ProductContext context)
+    {
+        _context = context;
+    }
+
+    // ... Existing code ...
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Product>> GetProducts()
+    {
+        var products = _context.Products.ToList();
+        return Ok(products);
+    }
+
+
+    [HttpGet("search")]
+    public ActionResult<IEnumerable<Product>> SearchProducts([FromQuery] string searchTerm)
+    {
+        if (string.IsNullOrEmpty(searchTerm))
+        {
+            return BadRequest("Search term is required.");
+        }
+
+        var matchingProducts = _context.Products
+            .Where(p => p.Name.Contains(searchTerm) || p.Description.Contains(searchTerm))
+            .ToList();
+
+        return Ok(matchingProducts);
+    }
+
+}
---
+++ Browse API/Data/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Browse_API.Data
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+        public int Stock {  get; set; }
+    }
+}
