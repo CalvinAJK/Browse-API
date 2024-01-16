261ccd0700fa05db32f7c35820b16cf61fcc70b6
CalvinAJK <calvinaujiakyn@gmail.com>
2024-01-16 07:01:46 +0000

    Minor fix

--- Browse API/Services/Products/ProductsServiceFake.cs
+++ Browse API/Services/Products/ProductsServiceFake.cs
@@ -4,9 +4,9 @@
     {
         private readonly ProductDTO[] _products =
         {
-            new ProductDTO { Name = "Fake product A", Description = "Fake Description A", Price = 11, Stock = true},
-            new ProductDTO { Name = "Fake product B", Description = "Fake Description B", Price = 22, Stock = true},
-            new ProductDTO { Name = "Fake product C", Description = "Fake Description C", Price = 33, Stock = false},
+            new ProductDTO { Name = "Fake product A", Description = "Fake Description A", Price = 11, InStock = true},
+            new ProductDTO { Name = "Fake product B", Description = "Fake Description B", Price = 22, InStock = true},
+            new ProductDTO { Name = "Fake product C", Description = "Fake Description C", Price = 33, InStock = false},
         };
 
         public Task<IEnumerable<ProductDTO>> GetProductsAsync()
